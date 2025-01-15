f4f6e46094c90d7309e73b34082507890045b076
Kshitij_Chaware <kshitijchaware@gmail.com>
2025-01-15 16:36:52 +0530

    Add files via upload

---
+++ my-app (Form)/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import FormContainer from "./components/FormContainer";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>React Form</h1>
+      <FormContainer />
+    </div>
+  );
+}
+
+export default App;
